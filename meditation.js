a60d3da9ac1ae4533ae34dfa8b00cfb73c06a74e
Arpit Jain <arpitjain070295@gmail.com>
2021-10-20 22:00:59 +0530

    Initial files

---
+++ meditation.js
@@ -0,0 +1,11 @@
+window.onload= function(){
+
+    const beach = document.querySelector('#beach');
+    const rain = document.querySelector('#rain');
+
+
+    rain.addEventListener('click', ()=>{
+        document.body.style.background = 'Video/rain.mp4'
+    })
+
+}
